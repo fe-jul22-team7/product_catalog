bfbc820ad951cbd97a394a3326d60b3611a13263
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-10-31 19:40:53 +0200

    added footer v. pre-Alpha

---
+++ product_catalog_client/src/components/footer/Footer.tsx
@@ -0,0 +1,26 @@
+import './Footer.scss';
+
+export const Footer = () => (
+  <div className='footer'>
+    <img
+      src='./images/Footer-logo.png'
+      alt="logo"
+      className='footer__logo'
+    />
+
+    <nav className='footer__nav'>
+      <ul className='footer__nav-list'>
+        <li className='footer__nav-item'>
+          <a href="" className='footer__nav-link'>Github</a>
+        </li>
+        <li className='footer__nav-item'>
+          <a href="" className='footer__nav-link'>Contacts</a>
+        </li>
+        <li className='footer__nav-item'>
+          <a href="" className='footer__nav-link'>Rights</a>
+        </li>
+      </ul>
+    </nav>
+    <a href="#" className='footer__backToTop-btn'>Back to top</a>
+  </div>
+);
