259e535601402419a8ce5ef7c4b137813aa948de
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-11-02 13:44:19 +0200

    burger menu

--- product_catalog_client/src/components/header/Header.tsx
+++ product_catalog_client/src/components/header/Header.tsx
@@ -6,32 +6,42 @@ import Cart from '../../images/Cart.svg';
 
 
 export const Header = () => (
-  <header
-    className="header"
-  > 
-    <div  className="header_block-logo-nav">
-      <a  className="header_logo">
-        <img 
+  <header className="header">
+    <div className="heade_mobile">
+      <a className="header_logo">
+        <img
           src={Logo}
           alt="logo"
         />
       </a>
-      <Navbar />
+      <a href='main-menu'> icon </a>
     </div>
-    
-    <div className="header_block-user">
-      <a  className="header_favorite header_user-item">
-        <img 
-          src={Favourite}
-          alt="Favourite"
-        />
-      </a>
-      <a  className="header_cart header_user-item">
-        <img 
-          src={Cart} 
-          alt="logo"
-        />
-      </a>
+    <div className="header_tible-desktop">
+      <div className="header_block-logo-nav">
+        <a className="header_logo">
+          <img
+            src={Logo}
+            alt="logo"
+          />
+        </a>
+        <Navbar />
+      </div>
+
+      <div className="header_block-user">
+        <a className="header_favorite header_user-item">
+          <img
+            src={Favourite}
+            alt="Favourite"
+          />
+        </a>
+        <a className="header_cart header_user-item">
+          <img
+            src={Cart}
+            alt="logo"
+          />
+        </a>
+      </div>
     </div>
+
   </header>
 );
--- product_catalog_client/src/components/header/Navbar.tsx
+++ product_catalog_client/src/components/header/Navbar.tsx
@@ -4,9 +4,10 @@ import './Navbar.scss';
 export const Navbar = () => (
   <nav
     data-cy="nav"
-    className="navbar is-fixed-top has-shadow"
+    className="navbar"
     role="navigation"
     aria-label="main navigation"
+    id="main-menu"
   >
     <div className="container">
       <div className="navbar-brand">
